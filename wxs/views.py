b221beecd82cf15d71614536a99e4c90f1da731d
beiqimoren <601059534@qq.com>
2024-04-24 12:46:12 +0800

    4月24日中午12时40分，配合APP完成登录注册模块功能联调，运行正常。版本为  开发板V424.运行正常！

--- wxs/views.py
+++ wxs/views.py
@@ -15,26 +15,53 @@ def index(request):
 
 
 # 用户登录验证
+# def login(request):
+#     username = request.GET['username']
+#     passored = request.GET['password']
+#     obj=models.UserInfo.objects.filter(username=username).first()
+#     response = HttpResponse("Hello, World!")
+#     response.status_code =222
+#     if obj.password == passored:
+#         response = HttpResponse(obj.id)
+#         response.status_code = 111
+#     return response
+
+
 def login(request):
     username = request.GET['username']
-    obj=models.UserInfo.objects.filter(username=username).first()
-    if obj.password == request.GET['password']:
+    password = request.GET['password']
+    if(models.UserInfo.objects.filter(username=username).exists()):
+        obj = models.UserInfo.objects.filter(username=username).first()
+        if(obj.password == password):
+            return JsonResponse({
+                'state': "成功",
+                'userID': obj.id
+            })
         return JsonResponse({
-            'state': "成功",
-            'userID': obj.id
+            'state': "密码错误",
+            'userID': 0
         })
-    return HttpResponse('失败')
+    return JsonResponse({
+        'state': "用户不存在",
+        'userID': 0
+    })
 
 
 # 注册用户
 def sigup(request):
     username = request.GET['username']
     password = request.GET['password']
-    if models.UserInfo.objects.filter(username=username).first():
-        return HttpResponse("该用户名已注册！")
+    if models.UserInfo.objects.filter(username=username).exists():
+        return JsonResponse({
+            'state': "该账号已注册",
+            'userID': 0
+        })
     models.UserInfo.objects.create(username=username, password=password)
-    ff = models.UserInfo.objects.all().values('username', 'password')
-    return HttpResponse(ff)
+    return JsonResponse({
+        'state': "注册成功",
+        'username': 0
+    })
+
 
 
 @require_http_methods(["POST"])
